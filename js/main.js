bc77b9fdb2c0f9239791f3dd65d91083a95239ae
[John Patterson] <pattersonjc1@gmail.com>
2016-12-30 12:35:43 -0500

    added favicon and project screenshots and descriptions

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 jQuery(function() {
 
 
+  $(document).scroll(function() {
+    if ($(this).scrollTop() > 10) { //Adjust 150
+      $('.header-container').addClass('shrinked');
+      $('.logo-header').addClass('shrinked');
+      $('.nav-links').addClass('shrinked');
+    } else {
+      $('.header-container').removeClass('shrinked');
+      $('.logo-header').removeClass('shrinked');
+      $('.nav-links').removeClass('shrinked');
+    }
+  });
+
+
+
+
 
 });
