6ebe7f6c07df77c2c65816de542bf853920f6f36
[John Patterson] <pattersonjc1@gmail.com>
2017-06-24 07:22:55 -0400

    Updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,34 +3,41 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
-// const autoprefixer = require('gulp-autoprefixer');
+const autoprefixer = require('gulp-autoprefixer');
+const babel = require("gulp-babel");
 
-gulp.task('sass', function() {
-  return gulp.src('./sass/*.scss')
+gulp.task('sass', () =>
+  gulp.src('./sass/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
-});
+    .pipe(gulp.dest('./dist/css'))
+    // .pipe(browserSync.reload({
+    //   stream: true
+    // }))
+);
 
-// gulp.task('autoprefixer', () =>
-//   gulp.src('./css/main.css')
-//   .pipe(autoprefixer({
-//     browsers: ['last 2 versions'],
-//     cascade: false
-//   }))
-//   .pipe(gulp.dest('./dist/css'))
-// );
-
-gulp.task('browserSync', function() {
+gulp.task('browserSync', () =>
   browserSync.init({
     server: {
       baseDir: ''
     },
   })
-})
+)
 
-gulp.task('default', ['browserSync', 'sass'], function() {
-  gulp.watch(['./sass/*.scss', '*.html', './js/*.js'], ['sass']);
-});
+gulp.task('autoprefixer', () =>
+  gulp.src('./sass/*.scss')
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('./dist/css'))
+);
+
+gulp.task('babel', () => 
+  gulp.src('./js/main.js')
+    .pipe(babel())
+    .pipe(gulp.dest('./dist/js'))
+);
+
+gulp.task('default', ['browserSync'], () => 
+  gulp.watch(['./sass/*.scss', '*.html', './js/*.js'], ['sass', 'autoprefixer', 'babel'])
+);
